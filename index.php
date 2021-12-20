05267c1d7c5b84f7ad79c1a5be0126b19b7d8d8b
ECLIPSIO <eclipsioz101@gmail.com>
2021-12-20 16:37:58 +0530

    Use index.php for test

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ use Classess\NumberHumanoid;
 
 include_once('./autoload.php');
 
+// For Million CHeck
 $num = new NumberHumanoid(1000221, 4);
 echo $num->out. "\t" .$num->amt. "\t" .$num->exp. "\r\n";
 
+//For thousands check
 $ab = new NumberHumanoid(1010, 2);
 echo $ab->out. "\t" .$ab->amt. "\t" .$ab->exp;
 exit;
