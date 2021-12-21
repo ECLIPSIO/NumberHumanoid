a300baa57bf50f06638b4858b0e2bf08e0e586ce
Naveen <naveen@internetbusinesssolutionsindia.com>
2021-12-21 13:13:47 +0530

    Only single Test Case

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ use Classes\NumberHumanoid;
 
 include_once('./autoload.php');
 
-// For Million CHeck
-$num = new NumberHumanoid(1020221, 2);
-echo $num->out. "\t" .$num->amt. "\t" .$num->exp. "\r\n";
-
 //For thousands check
 $ab = new NumberHumanoid(999991, 2);
 echo $ab->out. "\t" .$ab->amt. "\t" .$ab->exp;
