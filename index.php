2893036a610d9e051447cd931215dc6acefcf25b
Naveen <naveen@internetbusinesssolutionsindia.com>
2021-12-21 13:16:29 +0530

    Explanation about output

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ use Classes\NumberHumanoid;
 
 include_once('./autoload.php');
 
-//For thousands check
+//Test a number
+// out - Number with Identifier
+// amt - Number after conversion
+// exp - Range Identifier
 $ab = new NumberHumanoid(999991, 2);
 echo $ab->out. "\t" .$ab->amt. "\t" .$ab->exp;
 exit;
